2631b75ca0eb3dea73046e0bf3ba88e5d08153d6
seuriseuljjeok <heeseul09@naver.com>
2024-05-18 21:59:58 +0900

    [Setting] 스토리보드 삭제

--- 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/ViewController.swift
+++ 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/ViewController.swift
@@ -2,17 +2,17 @@
 //  ViewController.swift
 //  34th-SOPKATHON-Team1-iOS
 //
-//  Created by 박신영 on 5/16/24.
+//  Created by 윤희슬 on 5/18/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view.
-   }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
 
 
 }
