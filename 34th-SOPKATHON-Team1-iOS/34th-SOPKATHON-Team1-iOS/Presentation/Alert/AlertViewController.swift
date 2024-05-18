b4bdba2850f29951fbad5b9ba623fdd63c3411d8
seuriseuljjeok <heeseul09@naver.com>
2024-05-19 01:29:43 +0900

    [Setting] 폴더링

---
+++ 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/Presentation/Alert/AlertViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlertViewController.swift
+//  34th-SOPKATHON-Team1-iOS
+//
+//  Created by 윤희슬 on 5/19/24.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
