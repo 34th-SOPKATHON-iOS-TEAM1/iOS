85b1ad2f10c35b30fc78883eacbe9ea493ad58a3
ParkSY0919 <qhr498@gmail.com>
2024-05-19 05:49:56 +0900

    [Feat] Moya 기본 세팅 (#34)

---
+++ 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/Global/Setting/Config.swift
@@ -0,0 +1,35 @@
+//
+//  Config.swift
+//  34th-SOPKATHON-Team1-iOS
+//
+//  Created by 박신영 on 5/19/24.
+//
+
+import Foundation
+
+enum Config {
+    
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found.")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    
+    static let baseURL: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("Base URL is not set in plist for this configuration.")
+        }
+        return key
+    }()
+}
+
