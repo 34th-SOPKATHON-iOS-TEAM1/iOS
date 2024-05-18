c97abd648929ecafa31a05b8d3596160eb1d1699
seuriseuljjeok <heeseul09@naver.com>
2024-05-19 06:35:18 +0900

    [Feat] 모야 기본 세팅 (#37)

--- 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/SceneDelegate.swift
+++ 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // 2.
         self.window = UIWindow(windowScene: windowScene)
         // 3.
-        let navigationController = UINavigationController(rootViewController: RegisterNotToDoViewController())
+        let navigationController = UINavigationController(rootViewController: SplashViewController())
         navigationController.setNavigationBarHidden(true, animated: false)
         self.window?.rootViewController = navigationController
         // 4.
