89943181d4b7e581640e52bb0c4968dba7dfef15
ParkSY0919 <qhr498@gmail.com>
2024-05-19 06:21:31 +0900

    [Feat] 서버 api RequestBody 구현 (#38)

---
+++ 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/Network/RequestDTO/PostGoalRequestBody.swift
@@ -0,0 +1,14 @@
+//
+//  PostGoalRequestBody.swift
+//  34th-SOPKATHON-Team1-iOS
+//
+//  Created by 박신영 on 5/19/24.
+//
+
+import Foundation
+
+// MARK: - PostGoalRequestBody
+
+struct PostGoalRequestBody: Codable {
+    let goalContent: String
+}
---
+++ 34th-SOPKATHON-Team1-iOS/34th-SOPKATHON-Team1-iOS/Network/RequestDTO/PostNotTodoRequestBody.swift
@@ -0,0 +1,14 @@
+//
+//  PostNotTodoRequestBody.swift
+//  34th-SOPKATHON-Team1-iOS
+//
+//  Created by 박신영 on 5/19/24.
+//
+
+import Foundation
+
+// MARK: - PostNotTodoRequestBody
+
+struct PostNotTodoRequestBody: Codable {
+    let notTodoContent: [String]
+}
